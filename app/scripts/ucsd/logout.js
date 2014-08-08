a99861fcb2ee69773fe8572830bedf601fa03c70
Alex Wu <alex.h.wu@gmail.com>
2014-08-08 10:45:33 -0700

    Added initLogout to example page.

--- app/scripts/ucsd/logout.js
+++ app/scripts/ucsd/logout.js
@@ -3,7 +3,7 @@ function initLogout(logoutUrl) {
 	var url = "https://a4.ucsd.edu/tritON/resources/bugscript.jsp?target=https%3A%2F%2Fwww.ucsd.edu&jsoncallback=?";
 	$.getJSON(url, function(data) {
 		if (data.eduUcsdActLoggedin) {
-			var url = "<div id=\"tdr_login_content\">You are logged in | <a href=\"/security/logout?url=";
+			var url = "<div id=\"tdr_login_content\">You are logged in | <a href=\"https://act.ucsd.edu/security/logout?url=";
 			url += logoutUrl;
 			url += "\">Log Out</a></div>";
 			$("div#tdr_login").empty();
