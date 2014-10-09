1760088a98b269e7cfbc6a0eef689ab31121c829
Alex Wu <alex.h.wu@gmail.com>
2014-10-09 10:17:48 -0700

    Set explicit value for JavaScript detection of old IE.

--- app/scripts/ucsd/nav-search.js
+++ app/scripts/ucsd/nav-search.js
@@ -53,8 +53,13 @@ $(document).ready(function() {
     });
 
     function navMover() {
+        alert('calling navMover');
         if ($window.width() >= desktopBreak) {
+            alert('beyond breakpoint');
+
             if ($body.hasClass("active")) {
+                alert('has active class');
+
                 $body.removeClass("active");
 
                 if ($('#tdr_search_content>form').length) {
@@ -71,13 +76,16 @@ $(document).ready(function() {
             }
 
             if ($topNav.height() > maxNavHeight ) {
+                alert('nav to tall. collapsing');
                 $body.addClass('collapse-nav');
             } else {
+                alert('nav ok. remove collapse tag');
                 $body.removeClass('collapse-nav');
             }
         }
 
         if ($window.width() < desktopBreak && $body.hasClass("collapse-nav")) {
+            alert('remove collapse nav');
             $body.removeClass("collapse-nav");
         }
 
@@ -88,14 +96,14 @@ $(document).ready(function() {
     FastClick.attach(document.body);
 
     // Detecting IE
-    var oldIE;
+    var oldIE = false;
     if ($('html').is('.ie7, .ie8')) {
         oldIE = true;
     }
 
-    if (!oldIE) {
+    if (oldIE == false) {
+        alert('executing navMover');
         $window.on('load orientationchange resize', navMover);
     }
-
 
 });
