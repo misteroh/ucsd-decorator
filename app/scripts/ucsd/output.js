4651d9556778e698ef7843ff9ba6517882cac300
Jonathan Chang <jdkschang@gmail.com>
2015-06-18 15:57:21 -0700

    added calendar scss for better styling and added formatTime function using moment.js to parse date&time output separately

--- app/scripts/ucsd/output.js
+++ app/scripts/ucsd/output.js
@@ -1,13 +1,22 @@
 // inefficent, have to get and parse json file every time user clicks
 // build locally in array and parse array itself?
-$(document).ready(function() {
+
+function formatTime( rawDate ) {
+    var dateTimeOutput = [];
+
+    dateTimeOutput.push( moment( rawDate ).format( "dddd, MMMM Do YYYY" ));
+    dateTimeOutput.push( moment( rawDate ).format( "h:mm a" ));
+
+    return dateTimeOutput;
+}
+
+$(document).ready( function() {
     var jsonID,
-        eventID, eventName, eventDate,
-        outputID = $("#id"),
-        outputName = $("#name"),
-        outputDate = $("#date"),
-        url     = window.location.href,
-        grabID  = $(".cal");
+        eventID, eventName, eventDate, eventTime,
+        outputName = $("#name span"),
+        outputDate = $("#date span"),
+        outputTime = $("#time span"),
+        url     = window.location.href;
 
     //grabID.on( "click", function() {
     var parseRegex = new RegExp(/(?:(\?id=))\w+/g);
@@ -19,20 +28,18 @@ $(document).ready(function() {
 
     $.getJSON( "calendar.json", function( json ) {
         json.forEach(function (event) {
-            eventID   = event["id"];
-            eventName = event["name"];
-            eventDate = event["date"];
+            eventID   = event.id;
+            eventName = event.name;
+            eventDate = formatTime(event.date)[0];
+            eventTime = formatTime(event.date)[1];
 
             //console.log("eventID: " + eventID);
             //console.log("jsonID: " + jsonID);
             if ( eventID == jsonID ) {
-                console.log("eventID: " + eventID);
-                console.log("eventName: " + eventName);
-                console.log("eventDate: " + eventDate);
-
-                outputID.text("eventID: " + eventID);
-                outputName.text("eventName: " + eventName);
-                outputDate.text("eventDate: " + eventDate);
+                console.log("in conditional eventID");
+                outputName.append(eventName);
+                outputDate.append(eventDate);
+                outputTime.append(eventTime);
             }
         });
     });
