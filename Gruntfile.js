379931efb6cca02dd08d1f14deddd10b6ad05397
Jonathan Chang <jdkschang@gmail.com>
2015-06-26 11:27:46 -0700

    pathing for jquery in widget files and gruntfile formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,11 +230,7 @@ module.exports = function (grunt) {
             options: {
                 dest: '<%= config.dist %>'
             },
-            html:
-                [
-                    '<%= config.app %>/homepage-wide.html',
-                    '<%= config.app %>/widgets.html'
-                ]
+            html: [ '<%= config.app %>/homepage-wide.html', '<%= config.app %>/widgets.html' ]
         },
 
         // Performs rewrites based on rev and the useminPrepare configuration
